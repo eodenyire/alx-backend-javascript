da6be10f4a7df2883d8fc60be3af736df90e9739
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-09-30 21:24:11 +0300

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,10 +1,8 @@
-// Use const for task in taskFirst
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Use let for combination in taskNext
 export function getLast() {
   return ' is okay';
 }
@@ -12,5 +10,6 @@ export function getLast() {
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
+
   return combination;
 }
