4a6044c9be595bbcb62e6df087c7bccd6d7a420d
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-09-30 21:29:55 +0300

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,1 +1,10 @@
+// function to iterates through objects.
 
+export default function iterateThroughObject(reportWithIterator) {
+  // hope it works
+  const result = [];
+  for (const item of reportWithIterator) {
+    result.push(item);
+  }
+  return result;
+}
