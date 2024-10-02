c83c0f6d5a2a8968fde1850e7a0cd9caa4e94ea4
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-10-02 19:59:06 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-        .then((results) => {
-            const [photo, user] = results;
-            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  return uploadPhoto()
+    .then(({ body }) => {
+      createUser()
+        .then(({ firstName, lastName }) => {
+          console.log(`${body} ${firstName} ${lastName}`);
         })
         .catch(() => console.log('Signup system offline'));
+    })
+    .catch(() => console.log('Signup system offline'));
 }
