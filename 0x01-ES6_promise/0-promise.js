f912a1149f26a3c37d6943feadb7d45cd582a0a9
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-10-02 19:56:06 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-        resolve(); // Resolving the promise without any value
-    });
+  return new Promise(() => {});
 }
