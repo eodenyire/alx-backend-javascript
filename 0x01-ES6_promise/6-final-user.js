ccc4831ea599e69d2eb77c076318a916ed5ef909
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-10-02 20:01:58 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,21 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-all-reject';
+import uploadPhoto from './5-all-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-        .then((results) => results.map(result => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason,
-        })));
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const res = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    res.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    res.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
+  }
+  return res;
 }
