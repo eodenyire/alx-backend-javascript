d1423bb8cdbbfb8e5148bee30d64124ece53818d
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-10-02 20:03:24 +0300

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,12 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-        const result = mathFunction();
-        queue.push(result);
-    } catch (error) {
-        queue.push(`${error}`);
-    }
+  const queue = [];
+  try {
+    const res = mathFunction();
+    queue.push(res);
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
     queue.push('Guardrail was processed');
-    return queue;
+  }
+  return queue;
 }
