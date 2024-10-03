7f0e8aceedd3fea3a2649b0d5f609a30105ab202
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-10-03 20:03:11 +0300

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,1 +1,13 @@
+import Car from './10-car.js';
 
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // Override cloneCar to return a Car object instead of EVCar
+  cloneCar() {
+    return new Car();  // Returns a Car object
+  }
+}
