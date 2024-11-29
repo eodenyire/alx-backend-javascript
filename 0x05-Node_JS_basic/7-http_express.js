c70a53e89dc34d2ab60a8741672207806ff96d83
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-11-29 12:27:55 +0300

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,15 +2,29 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
+const port = 1245;
+
 app.get('/', (req, res) => res.send('Hello Holberton School!'));
+
 app.get('/students', async (req, res) => {
   const title = 'This is the list of our students\n';
+  const dbFile = process.argv[2]; // Get the database file from command line argument
+
+  if (!dbFile) {
+    res.status(500).send(`${title}Cannot load the database`);
+    return;
+  }
+
   try {
-    const data = await countStudents(process.argv[2]);
+    const data = await countStudents(dbFile);
     res.send(`${title}${data.join('\n')}`);
   } catch (error) {
-    res.send(`${title}${error.message}`);
+    res.status(500).send(`${title}${error.message}`);
   }
 });
-app.listen(1245);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
 module.exports = app;
